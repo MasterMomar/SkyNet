bf49cae4dfdaea3f69f53701bc7bb6fb0b828a47
Master Momar Mbergan <132151222+MasterMomar@users.noreply.github.com>
2023-09-10 08:17:28 -0700

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -17,7 +17,7 @@ function includeIfExists(string $file): ?ClassLoader
     return file_exists($file) ? include $file : null;
 }
 
-if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
+if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/MasterMomar/SkyNet/SkyNet/autoload.php'))) {
     echo 'You must set up the project dependencies using `composer install`'.PHP_EOL.
         'See https://getcomposer.org/download/ for instructions on installing Composer'.PHP_EOL;
     exit(1);
